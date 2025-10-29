bb21cb5b740efd40416b271236595b5a1225cdf1
Cauê.zx <caue61075@gmail.com>
2025-10-29 09:32:42 -0300

    Implement security checks and initialize settings
    Added security verification checks and loading functionality for the Blox Fruits script.

--- BloxFruits.lua
+++ BloxFruits.lua
@@ -1,3 +1,51 @@
+-- local function scary()
+--     warn("❌ This script must only be executed from the offical idk loader")
+--         return
+--     end
+
+-- local HttpService = game:GetService("HttpService")
+-- local SECRET_KEY = "YourSecretSecondaryToken" 
+-- local function simpleHMAC(input, key)
+--     return HttpService:UrlEncode(input .. key)
+-- end
+-- if not _G._secondaryData or
+--    not _G._secondaryData.nonce or 
+--    not _G._secondaryData.timestamp or 
+--    not _G._secondaryData.signature or 
+--    not _G._secondaryData.userKey or 
+--    not _G.__userKey then
+--     print("❌ Security verification failed: missing token data or user key.")
+--     scary()
+--     return
+-- end
+-- if _G._secondaryData.userKey ~= _G.__userKey then
+--     print("❌ Security verification failed: user key mismatch.")
+--     return
+-- end
+-- local tokenData = _G._secondaryData.nonce .. ":" .. _G._secondaryData.timestamp .. ":" .. _G.__userKey
+-- local expectedSignature = simpleHMAC(tokenData, SECRET_KEY)
+-- if expectedSignature ~= _G._secondaryData.signature then
+--     print("❌ Security verification failed: token signature mismatch.")
+--     return
+-- end
+-- local allowedTimeWindow = 300  -- seconds
+-- local currentTime = os.time()
+-- if math.abs(currentTime - tonumber(_G._secondaryData.timestamp)) > allowedTimeWindow then
+--     print("❌ Security verification failed: token expired.")
+--     return
+-- end
+-- _G._secondaryData = nil
+-- _G.__userKey = nil
+print("✅ Security verification passed. Loading IDk...") 
+if game.PlaceId == 2753915549 then
+	World1 = true;
+elseif game.PlaceId == 4442272183 then
+	World2 = true;
+elseif game.PlaceId == 7449423635 then
+	World3 = true;
+end;
+
+
 local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/tbao143/Library-ui/refs/heads/main/Redzhubui"))()
 
 local Window = redzlib:MakeWindow({
@@ -19,3 +67,435 @@ local Tab1 = Window:MakeTab({"🏡Home", "cherry"})
     Logo = "rbxassetid://18751483361",
     Invite = "Link discord invite",
 })
+
+local Library = Update:Window({
+	SubTitle = "Blox Fruits",
+	Size = UDim2.new(0, 450, 0, 300),
+	TabWidth = 140
+});
+local MainTab = Library:Tab("Main", "rbxassetid://10723407389");
+local FarmTab = Library:Tab("Farming", "rbxassetid://10723415335");
+local ItemsTab = Library:Tab("Items", "rbxassetid://10709782497");
+local SettingsTab = Library:Tab("Setting", "rbxassetid://10734950309");
+local LocalPlayerTab = Library:Tab("Local Player", "rbxassetid://10747373176");
+local HoldTab = Library:Tab("Hold Skill", "rbxassetid://10734984606");
+if World3 then
+	SeaTab = Library:Tab("Sea Event", "rbxassetid://10709761530");
+	SettingSeaTab = Library:Tab("Setting Sea", "rbxassetid://10709810948");
+end;
+if World2 or World3 then
+	SeaStackTab = Library:Tab("Stack Sea", "rbxassetid://10747376931");
+end;
+if World3 then
+	CraftTab = Library:Tab("Crafts", "rbxassetid://10723405360");
+	DragonDojoTab = Library:Tab("Dragon Dojo", "rbxassetid://10734951847");
+end;
+local StatsTab = Library:Tab("Stats Weapon", "rbxassetid://10709770317");
+if World3 or World2 then
+	RaceV4Tab = Library:Tab("Race V4", "rbxassetid://10723425539");
+end;
+local CombatTab = Library:Tab("Combat", "rbxassetid://10734975486");
+if World2 or World3 then
+	RaidTab = Library:Tab("Raid", "rbxassetid://10723345749");
+end;
+local EspTab = Library:Tab("Esp", "rbxassetid://10723346959");
+local TeleportTab = Library:Tab("Teleport", "rbxassetid://10734886004");
+local ShopTab = Library:Tab("Shop", "rbxassetid://10734952479");
+local FruitTab = Library:Tab("Devil Fruit", "rbxassetid://10734883986");
+local MiscTab = Library:Tab("Misc", "rbxassetid://10723424838");
+local ServTab = Library:Tab("Server", "rbxassetid://10723426722");
+_G.Settings = {
+	Main = { 
+    ["Select Weapon"] = "Melee",
+		["Farm Mode"] = "Normal",
+		["Auto Farm"] = false,
+		["Auto Farm Fast"] = false,
+		["Selected Mastery Mode"] = "Quest",
+		["Auto Farm Fruit Mastery"] = false,
+		["Auto Farm Gun Mastery"] = false,
+		["Selected Mastery Sword"] = nil,
+		["Auto Farm Sword Mastery"] = false,
+		["Selected Mob"] = nil,
+		["Auto Farm Mob"] = false,
+		["Selected Boss"] = nil,
+		["Auto Farm Boss"] = false,
+		["Auto Farm All Boss"] = false
+	},
+	Event = {},
+	Farm = {
+		["Auto Elite Hunter"] = false,
+		["Auto Elite Hunter Hop"] = false,
+		["Selected Bone Farm Mode"] = "Quest",
+		["Auto Farm Bone"] = false,
+		["Auto Random Surprise"] = false,
+		["Auto Pirate Raid"] = false,
+		["Auto Farm Observation"] = false,
+		["Auto Observation V2"] = false,
+		["Auto Musketeer Hat"] = false,
+		["Auto Saber"] = false,
+		["Auto Farm Chest Tween"] = false,
+		["Auto Farm Chest Instant"] = false,
+		["Auto Chest Hop"] = false,
+		["Auto Farm Chest Mirage"] = false,
+		["Auto Stop Items"] = false,
+		["Auto Farm Katakuri"] = false,
+		["Auto Spawn Cake Prince"] = false,
+		["Auto Kill Cake Prince"] = false,
+		["Auto Kill Dough King"] = false,
+		["Auto Farm Radioactive"] = false,
+		["Auto Farm Mystic Droplet"] = false,
+		["Auto Farm Magma Ore"] = false,
+		["Auto Farm Angel Wings"] = false,
+		["Auto Farm Leather"] = false,
+		["Auto Farm Scrap Metal"] = false,
+		["Auto Farm Conjured Cocoa"] = false,
+		["Auto Farm Dragon Scale"] = false,
+		["Auto Farm Gunpowder"] = false,
+		["Auto Farm Fish Tail"] = false,
+		["Auto Farm Mini Tusk"] = false
+	},
+	Setting = {
+		["Spin Position"] = false,
+		["Farm Distance"] = 35,
+		["Player Tween Speed"] = 350,
+		["Bring Mob"] = true,
+		["Bring Mob Mode"] = "Normal",
+		["Fast Attack"] = true,
+		["Fast Attack Mode"] = "Normal",
+		["Fast Attack Type"] = "New",
+		["Attack Aura"] = true,
+		["Hide Notification"] = false,
+		["Hide Damage Text"] = true,
+		["Black Screen"] = false,
+		["White Screen"] = false,
+		["Hide Monster"] = false,
+		["Mastery Health"] = 25,
+		["Fruit Mastery Skill Z"] = true,
+		["Fruit Mastery Skill X"] = true,
+		["Fruit Mastery Skill C"] = true,
+		["Fruit Mastery Skill V"] = false,
+		["Fruit Mastery Skill F"] = false,
+		["Gun Mastery Skill Z"] = true,
+		["Gun Mastery Skill X"] = true,
+		["Auto Set Spawn Point"] = true,
+		["Auto Observation"] = false,
+		["Auto Haki"] = true,
+		["Auto Rejoin"] = true,
+		["Bypass Anti Cheat"] = true
+	},
+	Hold = {
+		["Hold Mastery Skill Z"] = 0,
+		["Hold Mastery Skill X"] = 0,
+		["Hold Mastery Skill C"] = 0,
+		["Hold Mastery Skill V"] = 0,
+		["Hold Mastery Skill F"] = 0,
+		["Hold Sea Skill Z"] = 0,
+		["Hold Sea Skill X"] = 0,
+		["Hold Sea Skill C"] = 0,
+		["Hold Sea Skill V"] = 0,
+		["Hold Sea Skill F"] = 0 
+    },
+	Stats = {
+		["Auto Add Melee Stats"] = false,
+		["Auto Add Defense Stats"] = false,
+		["Auto Add Devil Fruit Stats"] = false,
+		["Auto Add Sword Stats"] = false,
+		["Auto Add Gun Stats"] = false,
+		["Point Stats"] = 1
+	},
+	Items = {
+		["Auto Second Sea"] = false,
+		["Auto Third Sea"] = false,
+		["Auto Farm Factory"] = false,
+		["Auto Super Human"] = false,
+		["Auto Death Step"] = false,
+		["Auto Fishman Karate"] = false,
+		["Auto Electric Claw"] = false,
+		["Auto Dragon Talon"] = false,
+		["Auto God Human"] = false,
+		["Auto Buddy Sword"] = false,
+		["Auto Soul Guitar"] = false,
+		["Auto Rengoku"] = false,
+		["Auto Hallow Scythe"] = false,
+		["Auto Warden Sword"] = false,
+		["Auto Cursed Dual Katana"] = false,
+		["Auto Yama"] = false,
+		["Auto Tushita"] = false,
+		["Auto Canvander"] = false,
+		["Auto Dragon Trident"] = false,
+		["Auto Pole"] = false,
+		["Auto Shawk Saw"] = false,
+		["Auto Greybeard"] = false,
+		["Auto Swan Glasses"] = false,
+		["Auto Arena Trainer"] = false,
+		["Auto Dark Dagger"] = false,
+		["Auto Press Haki Button"] = false,
+		["Auto Rainbow Haki"] = false,
+		["Auto Holy Torch"] = false,
+		["Auto Bartilo Quest"] = false
+	},
+	Esp = {
+		["ESP Player"] = false,
+		["ESP Chest"] = false,
+		["ESP DevilFruit"] = false,
+		["ESP RealFruit"] = false,
+		["ESP Flower"] = false,
+		["ESP Island"] = false,
+		["ESP Npc"] = false,
+		["ESP Sea Beast"] = false,
+		["ESP Monster"] = false,
+		["ESP Mirage"] = false,
+		["ESP Kitsune"] = false,
+		["ESP Frozen"] = false,
+		["ESP Advanced Fruit Dealer"] = false,
+		["ESP Aura"] = false,
+		["ESP Gear"] = false
+	},
+	DragonDojo = {
+		["Auto Farm Blaze Ember"] = false,
+		["Auto Collect Blaze Ember"] = false
+	},
+	SeaEvent = {
+		["Selected Boat"] = "Guardian",
+		["Selected Zone"] = "Zone 5",
+		["Boat Tween Speed"] = 300,
+		["Sail Boat"] = false,
+		["Auto Farm Shark"] = true,
+		["Auto Farm Piranha"] = true,
+		["Auto Farm Fish Crew Member"] = true,
+		["Auto Farm Ghost Ship"] = true,
+		["Auto Farm Pirate Brigade"] = true,
+		["Auto Farm Pirate Grand Brigade"] = true,
+		["Auto Farm Terrorshark"] = true,
+		["Auto Farm Seabeasts"] = true,
+		["Dodge Seabeasts Attack"] = true,
+		["Dodge Terrorshark Attack"] = true,
+		Lightning = false,
+		["Increase Boat Speed"] = false,
+		["No Clip Rock"] = false
+	},
+	SettingSea = {
+		["Skill Devil Fruit"] = true,
+		["Skill Melee"] = true,
+		["Skill Sword"] = true,
+		["Skill Gun"] = true,
+		["Sea Fruit Skill Z"] = true,
+		["Sea Fruit Skill X"] = true,
+		["Sea Fruit Skill C"] = true,
+		["Sea Fruit Skill V"] = false,
+		["Sea Fruit Skill F"] = false,
+		["Sea Melee Skill Z"] = true,
+		["Sea Melee Skill X"] = true,
+		["Sea Melee Skill C"] = true,
+		["Sea Melee Skill V"] = true,
+		["Sea Sword Skill Z"] = true,
+		["Sea Sword Skill X"] = true,
+		["Sea Gun Skill Z"] = true,
+		["Sea Gun Skill X"] = true
+	},
+	SeaStack = {
+		["Teleport To Frozen Dimension"] = false,
+		["Sail To Frozen Dimension"] = false,
+		["Summon Frozen Dimension"] = false,
+		["Teleport To Kitsune Island"] = false,
+		["Auto Collect Azure Ember"] = false,
+		["Set Azure Ember"] = 20,
+		["Auto Trade Azure Ember"] = false,
+		["Teleport To Mirage Island"] = false,
+		["Teleport To Advanced Fruit Dealer"] = false,
+		["Auto Attack Seabeasts"] = false,
+		["Summon Prehistoric Island"] = false,
+		["Tween To Prehistoric Island"] = false,
+		["Auto Kill Lava Golem"] = false
+	},
+	Craft = {
+		["Auto Craft Common Scroll"] = false,
+		["Auto Craft Rare Scroll"] = false,
+		["Auto Craft Legendary Scroll"] = false,
+		["Auto Craft Mythical Scroll"] = false
+	},
+	Race = {
+		["Auto Race V2"] = false,
+		["Auto Race V3"] = false,
+		["Selected PlaceV4"] = nil,
+		["Teleport To PlaceV4"] = false,
+		["Auto Buy Gear"] = false,
+		["Tween To Highest Mirage"] = false,
+		["Find Blue Gear"] = false,
+		["Look Moon Ability"] = false,
+		["Auto Train"] = false,
+		["Auto Kill Player After Trial"] = false,
+		["Auto Trial"] = false
+	},
+	Combat = {
+		["Auto Kill Player Quest"] = false,
+		["Aimbot Gun"] = false,
+		["Aimbot Skill Neares"] = false,
+		["Aimbot Skill"] = false,
+		["Enable PvP"] = false
+	},
+	Raid = {
+		["Selected Chip"] = nil,
+		["Auto Dungeon"] = false,
+		["Auto Awaken"] = false,
+		["Price Devil Fruit"] = 1000000,
+		["Unstore Devil Fruit"] = false,
+		["Law Raid"] = false
+	},
+	Shop = {
+		["Auto Buy Legendary Sword"] = false,
+		["Auto Buy Haki Color"] = false
+	},
+	LocalPlayer = {
+		["Infinite Energy"] = false,
+		["Infinite Ability"] = true,
+		["Infinite Geppo"] = false,
+		["Infinite Soru"] = false,
+		["Dodge No Cooldown"] = false,
+		["Active Race V3"] = false,
+		["Active Race V4"] = true,
+		["Walk On Water"] = true,
+		["No Clip"] = false
+	},
+	Fruit = {
+		["Auto Buy Random Fruit"] = false,
+		["Store Rarity Fruit"] = "Common - Mythical",
+		["Auto Store Fruit"] = false,
+		["Fruit Notification"] = false,
+		["Teleport To Fruit"] = false,
+		["Tween To Fruit"] = false
+	},
+	Misc = {
+		["Hide Chat"] = false,
+		["Hide Leaderboard"] = false,
+		["Highlight Mode"] = false
+	}
+};
+(getgenv()).Load = function()
+	if readfile and writefile and isfile and isfolder then
+		if not isfolder("NiceHub") then
+			makefolder("NiceHub");
+		end;
+		if not isfolder("Nice/Blox Fruits/") then
+			makefolder("Nice/Blox Fruits/");
+		end;
+		if not isfile(("NoxHub/Blox Fruits/" .. game.Players.LocalPlayer.Name .. ".json")) then
+			writefile("Nice/Blox Fruits/" .. game.Players.LocalPlayer.Name .. ".json", (game:GetService("HttpService")):JSONEncode(_G.Settings));
+		else
+			local Decode = (game:GetService("HttpService")):JSONDecode(readfile("NiceHub/Blox Fruits/" .. game.Players.LocalPlayer.Name .. ".json"));
+			for i, v in pairs(Decode) do
+				_G.Settings[i] = v;
+			end;
+		end;
+		print("Loaded!"); 
+    else
+		return warn("Status : Undetected Executor");
+	end;
+end;
+(getgenv()).SaveSetting = function()
+	if Update:SaveSettings() then
+		if readfile and writefile and isfile and isfolder then
+			if not isfile(("NoxHub/Blox Fruits/" .. game.Players.LocalPlayer.Name .. ".json")) then
+				(getgenv()).Load();
+			else
+				local Decode = (game:GetService("HttpService")):JSONDecode(readfile("NoxHub/Blox Fruits/" .. game.Players.LocalPlayer.Name .. ".json"));
+				local Array = {};
+				for i, v in pairs(_G.Settings) do
+					Array[i] = v;
+				end;
+				writefile("Nice/Blox Fruits/" .. game.Players.LocalPlayer.Name .. ".json", (game:GetService("HttpService")):JSONEncode(Array));
+			end;
+		else
+			return warn("Status : Undetected Executor");
+		end;
+	end;
+end;
+(getgenv()).Load();
+spawn(function()
+	repeat
+		task.wait();
+	until game:IsLoaded();
+	local ChatService = game:GetService("Chat");
+	wait(1);
+	((require(game.ReplicatedStorage.Notification)).new("<Color=Purple>[ Welcome " .. game.Players.LocalPlayer.DisplayName .. " ]<Color=/>")):Display();
+	wait(1);
+	((require(game.ReplicatedStorage.Notification)).new("<Color=Yellow>[ Enjoy for script ]<Color=/>")):Display();
+end);
+function CheckQuest()
+	MyLevel = (game:GetService("Players")).LocalPlayer.Data.Level.Value;
+	if World1 then
+		if MyLevel == 1 or MyLevel <= 9 then
+			Mon = "Bandit";
+			LevelQuest = 1;
+			NameQuest = "BanditQuest1";
+			NameMon = "Bandit";
+			CFrameQuest = CFrame.new(1059.37195, 15.4495068, 1550.4231, 0.939700544, -0, -0.341998369, 0, 1, -0, 0.341998369, 0, 0.939700544);
+			CFrameMon = CFrame.new(1045.962646484375, 27.00250816345215, 1560.8203125);
+		elseif MyLevel == 10 or MyLevel <= 14 then
+			Mon = "Monkey";
+			LevelQuest = 1;
+			NameQuest = "JungleQuest";
+			NameMon = "Monkey";
+			CFrameQuest = CFrame.new(-1598.08911, 35.5501175, 153.377838, 0, 0, 1, 0, 1, -0, -1, 0, 0);
+			CFrameMon = CFrame.new(-1448.51806640625, 67.85301208496094, 11.46579647064209);
+		elseif MyLevel == 15 or MyLevel <= 29 then
+			Mon = "Gorilla";
+			LevelQuest = 2;
+			NameQuest = "JungleQuest";
+			NameMon = "Gorilla";
+			CFrameQuest = CFrame.new(-1598.08911, 35.5501175, 153.377838, 0, 0, 1, 0, 1, -0, -1, 0, 0);
+			CFrameMon = CFrame.new(-1129.8836669921875, 40.46354675292969, -525.4237060546875);
+		elseif MyLevel == 30 or MyLevel <= 39 then
+			Mon = "Pirate";
+			LevelQuest = 1;
+			NameQuest = "BuggyQuest1";
+			NameMon = "Pirate";
+			CFrameQuest = CFrame.new(-1141.07483, 4.10001802, 3831.5498, 0.965929627, -0, -0.258804798, 0, 1, -0, 0.258804798, 0, 0.965929627);
+			CFrameMon = CFrame.new(-1103.513427734375, 13.752052307128906, 3896.091064453125);
+		elseif MyLevel == 40 or MyLevel <= 59 then
+			Mon = "Brute";
+			LevelQuest = 2;
+			NameQuest = "BuggyQuest1";
+			NameMon = "Brute";
+			CFrameQuest = CFrame.new(-1141.07483, 4.10001802, 3831.5498, 0.965929627, -0, -0.258804798, 0, 1, -0, 0.258804798, 0, 0.965929627);
+			CFrameMon = CFrame.new(-1140.083740234375, 14.809885025024414, 4322.92138671875);
+		elseif MyLevel == 60 or MyLevel <= 74 then
+			Mon = "Desert Bandit";
+			LevelQuest = 1;
+			NameQuest = "DesertQuest";
+			NameMon = "Desert Bandit";
+			CFrameQuest = CFrame.new(894.488647, 5.14000702, 4392.43359, 0.819155693, -0, -0.573571265, 0, 1, -0, 0.573571265, 0, 0.819155693);
+			CFrameMon = CFrame.new(924.7998046875, 6.44867467880249, 4481.5859375);
+		elseif MyLevel == 75 or MyLevel <= 89 then
+			Mon = "Desert Officer";
+			LevelQuest = 2;
+			NameQuest = "DesertQuest";
+			NameMon = "Desert Officer";
+			CFrameQuest = CFrame.new(894.488647, 5.14000702, 4392.43359, 0.819155693, -0, -0.573571265, 0, 1, -0, 0.573571265, 0, 0.819155693);
+			CFrameMon = CFrame.new(1608.2822265625, 8.614224433898926, 4371.00732421875);
+		elseif MyLevel == 90 or MyLevel <= 99 then
+			Mon = "Snow Bandit";
+			LevelQuest = 1;
+			NameQuest = "SnowQuest";
+			NameMon = "Snow Bandit";
+			CFrameQuest = CFrame.new(1389.74451, 88.1519318, -1298.90796, -0.342042685, 0, 0.939684391, 0, 1, 0, -0.939684391, 0, -0.342042685);
+			CFrameMon = CFrame.new(1354.347900390625, 87.27277374267578, -1393.946533203125);
+		elseif MyLevel == 100 or MyLevel <= 119 then
+			Mon = "Snowman";
+			LevelQuest = 2;
+			NameQuest = "SnowQuest";
+			NameMon = "Snowman";
+			CFrameQuest = CFrame.new(1389.74451, 88.1519318, -1298.90796, -0.342042685, 0, 0.939684391, 0, 1, 0, -0.939684391, 0, -0.342042685);
+			CFrameMon = CFrame.new(1201.6412353515625, 144.57958984375, -1550.0670166015625);
+		elseif MyLevel == 120 or MyLevel <= 149 then
+			Mon = "Chief Petty Officer";
+			LevelQuest = 1;
+			NameQuest = "MarineQuest2";
+			NameMon = "Chief Petty Officer";
+			CFrameQuest = CFrame.new(-5039.58643, 27.3500385, 4324.68018, 0, 0, -1, 0, 1, 0, 1, 0, 0);
+			CFrameMon = CFrame.new(-4881.23095703125, 22.65204429626465, 4273.75244140625);
+		elseif MyLevel == 150 or MyLevel <= 174 then
+			Mon = "Sky Bandit";
+			LevelQuest = 1;
+			NameQuest = "SkyQuest";
+			NameMon = "Sky Bandit";
