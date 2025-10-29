074d2431f8e95a3848fea3fcf81bee8f32dee599
Cauê.zx <caue61075@gmail.com>
2025-10-29 09:43:16 -0300

    Remove unnecessary blank lines in BloxFruits.lua

--- BloxFruits.lua
+++ BloxFruits.lua
@@ -1,12 +1,9 @@
 local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/tbao143/Library-ui/refs/heads/main/Redzhubui"))()
-
 local Window = redzlib:MakeWindow({
   Title = "redz Hub : Blox Fruits",
   SubTitle = "by redz9999",
   SaveFolder = "testando | redz lib v5.lua"
 }) 
-
-
 Tab1:AddDiscordInvite({
     Name = "Name Hub",
     Description = "Join server",
