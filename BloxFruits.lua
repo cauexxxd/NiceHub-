7d3251eab083e18d72c6e410f0174256144be194
Cauê.zx <caue61075@gmail.com>
2025-10-29 10:12:11 -0300

    Update BloxFruits.lua

--- BloxFruits.lua
+++ BloxFruits.lua
@@ -14,17 +14,7 @@ Icon = <string> - URL to the image you want displayed on the window.
 CloseCallback = <function> - Function to execute when the window is closed.
 ]] 
 
-local Tab = Window:MakeTab({
-	Name = "Home ",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
 
---[[
-Name = <string> - The name of the tab.
-Icon = <string> - The icon of the tab.
-PremiumOnly = <bool> - Makes the tab accessible to Sirus Premium users only.
-]]
 OrionLib:MakeNotification({
 	Name = "carregando!",
 	Content = "Notification content... what will it say??",
@@ -38,3 +28,198 @@ Content = <string> - The content of the notification.
 Image = <string> - The icon of the notification.
 Time = <number> - The duration of the notfication.
 ]]
+Tab:AddSlider({
+	Name = "Slider",
+	Min = 0,
+	Max = 20,
+	Default = 5,
+	Color = Color3.fromRGB(255,255,255),
+	Increment = 1,
+	ValueName = "bananas",
+	Callback = function(Value)
+		print(Value)
+	end    
+})
+
+--[[
+Name = <string> - The name of the slider.
+Min = <number> - The minimal value of the slider.
+Max = <number> - The maxium value of the slider.
+Increment = <number> - How much the slider will change value when dragging.
+Default = <number> - The default value of the slider.
+ValueName = <string> - The text after the value number.
+Callback = <function> - The function of the slider.
+] 
+
+if Update:LoadAnimation() then
+	Update:StartLoad();
+end;
+if Update:LoadAnimation() then
+	Update:Loaded();
+end;
+local Library = Update:Window({
+	SubTitle = "Blox Fruits",
+	Size = UDim2.new(0, 450, 0, 300),
+	TabWidth = 140
+});
+local MainTab = Library:Tab("Main", "rbxassetid://10723407389");
+local FarmTab = Library:Tab("Farming", "rbxassetid://10723415335");
+local ItemsTab = Library:Tab("Items", "rbxassetid://10709782497");
+local SettingsTab = Library:Tab("Setting", "rbxassetid://10734950309");
+local LocalPlayerTab = Library:Tab("Local Player", "rbxassetid://10747373176");
+local HoldTab = Library:Tab("Hold Skill", "rbxassetid://10734984606");
+if World3 then
+	SeaTab = Library:Tab("Sea Event", "rbxassetid://10709761530");
+	SettingSeaTab = Library:Tab("Setting Sea", "rbxassetid://10709810948");
+end;
+if World2 or World3 then
+	SeaStackTab = Library:Tab("Stack Sea", "rbxassetid://10747376931");
+end;
+if World3 then
+	CraftTab = Library:Tab("Crafts", "rbxassetid://10723405360");
+	DragonDojoTab = Library:Tab("Dragon Dojo", "rbxassetid://10734951847");
+end;
+local StatsTab = Library:Tab("Stats Weapon", "rbxassetid://10709770317");
+if World3 or World2 then
+	RaceV4Tab = Library:Tab("Race V4", "rbxassetid://10723425539");
+end;
+local CombatTab = Library:Tab("Combat", "rbxassetid://10734975486");
+if World2 or World3 then
+	RaidTab = Library:Tab("Raid", "rbxassetid://10723345749");
+end;
+local EspTab = Library:Tab("Esp", "rbxassetid://10723346959");
+local TeleportTab = Library:Tab("Teleport", "rbxassetid://10734886004");
+local ShopTab = Library:Tab("Shop", "rbxassetid://10734952479");
+local FruitTab = Library:Tab("Devil Fruit", "rbxassetid://10734883986");
+local MiscTab = Library:Tab("Misc", "rbxassetid://10723424838");
+local ServTab = Library:Tab("Server", "rbxassetid://10723426722");
+_G.Settings = {
+	Main = { 
+		["Auto Farm Pirate Brigade"] = true,
+		["Auto Farm Pirate Grand Brigade"] = true,
+		["Auto Farm Terrorshark"] = true,
+		["Auto Farm Seabeasts"] = true,
+		["Dodge Seabeasts Attack"] = true,
+		["Dodge Terrorshark Attack"] = true,
+		Lightning = false,
+		["Increase Boat Speed"] = false,
+		["No Clip Rock"] = false
+	},
+	SettingSea = {
+		["Skill Devil Fruit"] = true,
+		["Skill Melee"] = true,
+		["Skill Sword"] = true,
+		["Skill Gun"] = true,
+		["Sea Fruit Skill Z"] = true,
+		["Sea Fruit Skill X"] = true,
+		["Sea Fruit Skill C"] = true,
+		["Sea Fruit Skill V"] = false,
+		["Sea Fruit Skill F"] = false,
+		["Sea Melee Skill Z"] = true,
+		["Sea Melee Skill X"] = true,
+		["Sea Melee Skill C"] = true,
+		["Sea Melee Skill V"] = true,
+		["Sea Sword Skill Z"] = true,
+		["Sea Sword Skill X"] = true,
+		["Sea Gun Skill Z"] = true,
+		["Sea Gun Skill X"] = true
+	},
+	SeaStack = {
+		["Teleport To Frozen Dimension"] = false,
+		["Sail To Frozen Dimension"] = false,
+		["Summon Frozen Dimension"] = false,
+		["Teleport To Kitsune Island"] = false,
+		["Auto Collect Azure Ember"] = false,
+		["Set Azure Ember"] = 20,
+		["Auto Trade Azure Ember"] = false,
+		["Teleport To Mirage Island"] = false,
+		["Teleport To Advanced Fruit Dealer"] = false,
+		["Auto Attack Seabeasts"] = false,
+		["Summon Prehistoric Island"] = false,
+		["Tween To Prehistoric Island"] = false,
+		["Auto Kill Lava Golem"] = false
+	},		["ESP Monster"] = false,
+		["ESP Mirage"] = false,
+		["ESP Kitsune"] = false,
+		["ESP Frozen"] = false,
+		["ESP Advanced Fruit Dealer"] = false,
+		["ESP Aura"] = false,
+		["ESP Gear"] = false
+	},
+	DragonDojo = {
+		["Auto Farm Blaze Ember"] = false,
+		["Auto Collect Blaze Ember"] = false
+	},
+	SeaEvent = {
+		["Selected Boat"] = "Guardian",
+		["Selected Zone"] = "Zone 5",
+		["Boat Tween Speed"] = 300,
+		["Sail Boat"] = false,
+		["Auto Farm Shark"] = true,
+		["Auto Farm Piranha"] = true,
+		["Auto Farm Fish Crew Member"] = true,
+		["Auto Farm Ghost Ship"] = true,
+
+	Craft = {
+		["Auto Craft Common Scroll"] = false,
+		["Auto Craft Rare Scroll"] = false,
+		["Auto Craft Legendary Scroll"] = false,
+		["Auto Craft Mythical Scroll"] = false
+	},
+	Race = {
+		["Auto Race V2"] = false,
+		["Auto Race V3"] = false,
+		["Selected PlaceV4"] = nil,
+		["Teleport To PlaceV4"] = false,
+		["Auto Buy Gear"] = false,
+		["Tween To Highest Mirage"] = false,
+		["Find Blue Gear"] = false,
+		["Look Moon Ability"] = false,
+		["Auto Train"] = false,
+		["Auto Kill Player After Trial"] = false,
+		["Auto Trial"] = false
+	},
+	Combat = {
+		["Auto Kill Player Quest"] = false,
+		["Aimbot Gun"] = false,
+		["Aimbot Skill Neares"] = false,
+		["Aimbot Skill"] = false,
+		["Enable PvP"] = false
+	},
+	Raid = {
+		["Selected Chip"] = nil,
+		["Auto Dungeon"] = false,
+		["Auto Awaken"] = false,
+		["Price Devil Fruit"] = 1000000,
+		["Unstore Devil Fruit"] = false,
+		["Law Raid"] = false
+	},
+	Shop = {
+		["Auto Buy Legendary Sword"] = false,
+		["Auto Buy Haki Color"] = false
+	},
+	LocalPlayer = {
+		["Infinite Energy"] = false,
+		["Infinite Ability"] = true,
+		["Infinite Geppo"] = false,
+		["Infinite Soru"] = false,
+		["Dodge No Cooldown"] = false,
+		["Active Race V3"] = false,
+		["Active Race V4"] = true,
+		["Walk On Water"] = true,
+		["No Clip"] = false
+	},
+	Fruit = {
+		["Auto Buy Random Fruit"] = false,
+		["Store Rarity Fruit"] = "Common - Mythical",
+		["Auto Store Fruit"] = false,
+		["Fruit Notification"] = false,
+		["Teleport To Fruit"] = false,
+		["Tween To Fruit"] = false
+	},
+	Misc = {
+		["Hide Chat"] = false,
+		["Hide Leaderboard"] = false,
+		["Highlight Mode"] = false
+	}
+};
